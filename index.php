aeae394547815bac088d9f29a12671ba8518ad29
Matthew-Riddell <d00245674@student.dkit.ie>
2023-02-20 11:00:01 +0000

    NavBar as a seperate file

--- index.php
+++ index.php
@@ -24,32 +24,7 @@ $statement->closeCursor();
   </head>
   <body>
     
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">Site Title</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse" id="navbarsExample07">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          </ul>
-          <span class="d-flex">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="contact-form.html">Contact Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-2.html">Page 2</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-3.html">Page 3</a>
-              </li>
-            </ul>
-          </span>
-        </div>
-      </div>
-    </nav>
+	<?php include('nav.html'); ?>
 
 <main class="container">
   <div class="starter-template text-center">
